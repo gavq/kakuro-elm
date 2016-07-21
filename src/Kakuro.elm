eab853a88c1770335bea530d87a9c3795b5d6c96
James Petry <gavilancomun@gmail.com>
2016-07-21 10:41:52 +0100

    wip

--- src/Kakuro.elm
+++ src/Kakuro.elm
@@ -1,21 +1,26 @@
 module Kakuro exposing (..)
 
-type alias EmptyCell = {}
-type alias AcrossCell = { across: Int }
-type alias DownCell = { down: Int }
-type alias DownAcrossCell = { down: Int, across: Int }
+import String
+
+type alias Across a = {a | across: Int}
+type alias Down a = {a | down: Int}
+type alias DownCell = Down {}
+type alias AcrossCell = Across {}
+type alias DownAcrossCell = Down(Across {})
+
+type alias EmptyCell = { empty: Int }
 type alias ValueCell = { values: List Int }
 
 draw cell =
   case cell of
-    .down && .across -> " " ++ down ++ "//" ++ across ++ " "
-    .down -> " " ++ down ++ "//-- "
-    .across -> " --//" ++ across ++ " "
-    .values -> values
+    {down, across} -> " " ++ down ++ "//" ++ across ++ " "
+    {down} -> " " ++ down ++ "//-- "
+    {across} -> " --//" ++ across ++ " "
+    {values} -> String.concat values
     _ -> "-----"
 
-e = {}
-v = {value = [1, 2, 3, 4, 5, 6, 7, 8, 9]}
+e = {empty = 0}
+v = {values = [1, 2, 3, 4, 5, 6, 7, 8, 9]}
 a across = {across = across}
 d down = {down = down}
 da down across = {down = down, across = across}
